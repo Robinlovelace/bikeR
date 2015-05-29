60a951e35383f3d11c0c7f81f9edee84f8cd4801
Robinlovelace <rob00x@gmail.com>
2015-05-29 18:55:29 +0100

    Hotspot analysis

---
+++ video-routes/hotspot-routes.R
@@ -0,0 +1,5 @@
+# Aim: to identify commuting accident hotspots and routes to get there
+
+# source("video-routes/load-stats19.R") # load the data
+
+
---
+++ video-routes/load-stats19.R
@@ -0,0 +1,96 @@
+# Aim: load, clean and subset STATS19 data
+
+pkgs <- c("rgdal", "downloader", "lubridate", "ggplot2", "dplyr")
+lapply(pkgs, library, character.only = T)
+
+# # Download latest tables (not raw data) not road traffic casualties
+# download("https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/401298/quarterly-estimates-jul-to-sep-2014.zip", destfile = "stats19-2014.zip")
+# unzip("stats19-2014.zip", exdir = "stats19data")
+#
+# # Download latest stats19 data
+# download(url = "http://data.dft.gov.uk/road-accidents-safety-data/Stats19-Data2005-2013.zip", destfile = "Stats19-Data2005-2013.zip")
+# unzip("Stats19-Data2005-2013.zip", exdir = "stats19data")
+
+# ac <- read.csv("stats19data/Accidents0513.csv")
+# vt <- read.csv("stats19data/Vehicles0513.csv")
+# ca <- read.csv("stats19data/Casualties0513.csv") # casualties
+#
+# # Preliminary analysis
+# length(unique(vt$Acc_Index))
+#
+# # Extract bike crashes
+# head(vt$Vehicle_Type)
+# cyCodes <- vt$Acc_Index[vt$Vehicle_Type == 1]
+# # are they a cyclist?
+# ac$cyclist <- "No cyclist"
+# ac$cyclist[cyCodes] <- "Cyclist"
+# ac$cyclist <- factor(ac$cyclist)
+# summary(ac$cyclist)
+# source("stat19/addFactors.R") # add factors to data
+# save(ac, vt, ca, file = "stats19data/2013-alldata.RData")
+
+cyVt <- vt[ vt$Acc_Index %in% cyCodes, ]
+cyAc <- ac[ ac$Accident_Index %in% cyCodes, ]
+nrow(cyVt)
+
+head(cyVt)
+plot(cyAc$Location_Easting_OSGR, cyAc$Location_Northing_OSGR)
+names(cyAc)
+cyAc.mini <- cyAc[c(2,3,6,7,10)]
+object.size(cyAc.mini) / 1000000
+head(cyAc.mini)
+save(cyAc.mini, file="/media//SAMSUNG/repos/osm-cycle/cy-uptake/updata/cyAc.mini.STATS19.RData")
+
+# Subset to Leeds area, then save and play
+# counties <- readOGR(dsn="/scratch/gdata/" , layer="england_ct_2011_gen_clipped")
+# counties <- readOGR(dsn="/media/" , layer="england_ct_2011_gen_clipped")
+# plot(counties)
+# head(counties@data)
+# WY <- counties[ counties$NAME == "West Yorkshire", ]
+# WY <- wy # if already loaded under different name
+plot(WY)
+save(WY, file = "geodata/WYoutline.RData")
+# convert Time text to chron format
+library(chron)
+ac$time <- as.character(ac$Time)
+ac$time <- paste0(ac$time,":00")
+ac$time <- chron(times = ac$time)
+hist(ac$time)
+
+# same for dates
+head(ac$Date)
+ac$date <- as.character(ac$Date)
+ac$date <- as.Date(ac$date, format="%d/%m/%Y")
+summary(ac$date)
+plot(ac$date[1:1000])
+library(ggplot2)
+qplot(ac$date, geom="histogram", binwidth = 30) # ...
+
+
+
+
+bbox(WY)
+WY <- spTransform(WY, CRSobj = CRS("+init=epsg:27700"))
+bbox(WY)
+
+summary(ac$Location_Easting_OSGR)
+ac <- ac[-which(is.na(ac$Location_Easting_OSGR)), ]
+ac <- SpatialPointsDataFrame(coords=matrix(c(ac$Location_Easting_OSGR, ac$Location_Northing_OSGR), ncol=2), data=ac)
+proj4string(ac) <- proj4string(WY)
+
+### RUN addFactors here ### and then continue
+
+# all zones in WY
+plot(WY)
+acWY <- ac[WY,]
+head(acWY@data)
+plot(acWY)
+acWY$Date[sample(1:nrow(acWY), size=50)]
+
+## Extract dates from data
+acWY
+object.size(acWY)/1000000
+object.size(ac)/1000000
+vtWY <- vt[ vt$Acc_Index %in% acWY$Accident_Index, ]
+caWY <- ca[ ca$Acc_Index %in% acWY$Accident_Index, ]
+save(caWY, vtWY, acWY, file="WYall.RData")
